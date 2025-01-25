34963bc2bb2e4e9c495c31574dd323d5f32760d3
Abrar Yeasir <iamabraryeasir@gmail.com>
2025-01-25 19:25:18 +0600

    set upped the app and the global middlewares for making the app more production grade.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,13 @@
 import "dotenv/config";
 
 // dependency imports
+import app from "./app.js";
 import connectToDB from "./db/index.js";
 
 // connecting to db
-connectToDB();
+connectToDB().then(() => {
+  const port = process.env.PORT || 8000;
+  app.listen(port, () => {
+    console.log(`Server is running at port => ${port}`);
+  });
+});
