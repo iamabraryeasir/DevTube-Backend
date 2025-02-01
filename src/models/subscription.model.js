ade3974d6c9c193a1549f6eeeb0b1547fb0a3fa7
Abrar Yeasir <iamabraryeasir@gmail.com>
2025-02-01 08:05:32 +0600

    feat(models): add subscription model and fix user controller bug
    - Add new subscription model for handling user subscriptions
    - Fix bug in user controller

---
+++ src/models/subscription.model.js
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose";
+
+const subscriptionSchema = new Schema(
+  {
+    user: {
+      // user who is subscribed
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    channel: {
+      // channel to which the user is subscribed
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+export const Subscription = model("Subscription", subscriptionSchema);
