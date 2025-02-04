8e427f0bd8bf12745a46b281e327cc2af3cc8680
Abrar Yeasir <iamabraryeasir@gmail.com>
2025-02-04 15:34:16 +0600

    fix(models): update comment for user field in subscription model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -3,10 +3,11 @@ import { Schema, model } from "mongoose";
 const subscriptionSchema = new Schema(
   {
     user: {
-      // user who is subscribed
+      // user who is subscribing
       type: Schema.Types.ObjectId,
       ref: "User",
     },
+
     channel: {
       // channel to which the user is subscribed
       type: Schema.Types.ObjectId,
